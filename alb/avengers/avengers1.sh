b14496f2299ca3fcbb520f1e338a466ee064c59b
rajscool095 <74223488+rajscool095@users.noreply.github.com>
2020-11-30 13:39:01 +0530

    Update avengers1.sh

--- alb/avengers/avengers1.sh
+++ alb/avengers/avengers1.sh
@@ -3,7 +3,7 @@ yum install -y httpd php git
 
 cd /var/www/html
 wget https://github.com/rajscool095/rvemula/blob/main/alb/avengers/avengers1.png
-mv avebgers1.png avengers.jpg
+mv avengers1.png avengers.jpg
 wget https://github.com/rajscool095/rvemula/blob/main/alb/avengers/index.php
 mv /var/www/html/htaccess /var/www/html/.htaccess
 
