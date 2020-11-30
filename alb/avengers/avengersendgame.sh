f9073c16868729522e3f9974271ab2077dad0e66
rajscool095 <74223488+rajscool095@users.noreply.github.com>
2020-11-30 13:08:18 +0530

    Add files via upload

---
+++ alb/avengers/avengersendgame.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+yum install -y httpd php git
+
+cd /var/www/html
+wget https://github.com/rajscool095/rvemula/blob/main/alb/avengers/avengersendgame.png
+mv avengersendgame.png avengers.jpg
+wget https://github.com/rajscool095/rvemula/blob/main/alb/avengers/index.php
+mv /var/www/html/htaccess /var/www/html/.htaccess
+
+mkdir /var/www/html/avengers
+cp /var/www/html/avengers.jpg /var/www/html/avengers
+cp /var/www/html/index.php /var/www/html/avengers
+cp /var/www/html/.htaccess /var/www/html/avengers
+
+
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
