bf8ac5a4a7e49fea8c2fe71deca5563e8f03e04e
rajscool095 <74223488+rajscool095@users.noreply.github.com>
2020-11-30 13:15:52 +0530

    Add files via upload

---
+++ alb/jl/JL2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+yum install -y httpd php git
+
+cd /var/www/html
+wget https://github.com/rajscool095/rvemula/blob/main/alb/jl/JL2.png
+mv JL2.png justiceleague.jpg
+wget https://github.com/rajscool095/rvemula/blob/main/alb/jl/index.php
+mv /var/www/html/htaccess /var/www/html/.htaccess
+
+mkdir /var/www/html/jl
+cp /var/www/html/jl.jpg /var/www/html/jl
+cp /var/www/html/index.php /var/www/html/jl
+cp /var/www/html/.htaccess /var/www/html/jl
+
+
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
