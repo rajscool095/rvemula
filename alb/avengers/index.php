e3c9939a55e94b0281eb7bd792b65b9d3c5d96df
rajscool095 <74223488+rajscool095@users.noreply.github.com>
2020-12-08 21:21:10 +0530

    Update index.php

--- alb/avengers/index.php
+++ alb/avengers/index.php
@@ -3,7 +3,7 @@
 <body>
 <h1>
 <center>
-<IMG SRC="avengers.jpg" ALT="omg"><br>
+<IMG SRC="avengers.jpg" ALT="error downloading file"><br>
 <?php
 $eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
 echo $eip;
