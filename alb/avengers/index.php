02199310727a35f1ca6bcb919f6fdffe9913df43
rajscool095 <74223488+rajscool095@users.noreply.github.com>
2023-06-02 15:34:18 +0530

    Update index.php
    changed to metadata v2

--- alb/avengers/index.php
+++ alb/avengers/index.php
@@ -5,7 +5,7 @@
 <center>
 <IMG SRC="avengers.jpg" ALT="error downloading file"><br>
 <?php
-$eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
+$eip = file_get_contents('TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-ipv4');
 echo $eip;
 ?>
 </center>
